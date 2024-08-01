427ccdd16624bb3b4fb16ebb338ccf1c599e527b
Snehal Satape <139950388+Snehalsatape@users.noreply.github.com>
2024-08-01 16:03:13 +0530

    Add files via upload

---
+++ javascript/newasign.js
@@ -0,0 +1,19 @@
+// function factorial(num) {
+//     if (num === 0 || num === 1)
+//         return 1;
+//     for (var i = num - 1; i >= 1; i--) {
+//         num *= i;
+//     }
+//     return num;
+// }
+
+// var number = 5; // You can change this number to calculate the factorial of a different number
+// var result = factorial(number);
+// console.log("The factorial of", number, "is", result);
+
+age = int(input("Enter you age: "))
+
+if age>= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
---
+++ javascript/practise.js
@@ -0,0 +1,33 @@
+// for(let i=1;i<=10;i++){
+//     console.log("7 X " + i + "=" + 7*i)
+    
+// }
+
+// for (let i=1; i<=10;i++)
+// {
+//     console.log(" +" + i + "=" + i+i )
+
+// }
+
+
+
+// n1=0
+// n2=1
+// {for (n1=1;n1<=10;n1++)
+// console.log(n1+n2)
+// n3=n1+n2
+// console.log(n3)
+
+
+var a
+console.log(a)
+console.log(b)
+
+oreo = {
+    name:"oreo",
+    age: 13,
+    gender:"female",
+   
+}
+    
+console.log(oreo)
